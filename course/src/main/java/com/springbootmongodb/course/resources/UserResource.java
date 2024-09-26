b88f7a4c9caf498d89c5ce89387380d5f973a966
Gabriel Xavier <gabrielgx928@gmail.com>
2024-09-26 11:30:00 -0300

    Endpoint para retornar os posts de um usuario

--- course/src/main/java/com/springbootmongodb/course/resources/UserResource.java
+++ course/src/main/java/com/springbootmongodb/course/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.springbootmongodb.course.resources;
 
+import com.springbootmongodb.course.domain.Post;
 import com.springbootmongodb.course.domain.User;
 import com.springbootmongodb.course.dto.UserDTO;
 import com.springbootmongodb.course.services.UserService;
@@ -55,5 +56,11 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
