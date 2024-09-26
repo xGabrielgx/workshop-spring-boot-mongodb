ba5d45481117d5b33023848a2ca4020e53d527ed
Gabriel Xavier <gabrielgx928@gmail.com>
2024-09-26 15:59:55 -0300

    Consulta simples com @Query

--- course/src/main/java/com/springbootmongodb/course/repository/PostRepository.java
+++ course/src/main/java/com/springbootmongodb/course/repository/PostRepository.java
@@ -2,12 +2,18 @@ package com.springbootmongodb.course.repository;
 
 import com.springbootmongodb.course.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    // metódo com @query para consultar direto com nomeclatura do MongoDB
+    // ?0 primeiro parametro que vier no método
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
 
     // query methods
     List<Post> findByTitleContainingIgnoreCase(String text);
--- course/src/main/java/com/springbootmongodb/course/services/PostService.java
+++ course/src/main/java/com/springbootmongodb/course/services/PostService.java
@@ -26,6 +26,6 @@ public class PostService {
     }
 
     public List<Post> findByTitle(String text) {
-        return repository.findByTitleContainingIgnoreCase(text);
+        return repository.searchTitle(text);
     }
 }
